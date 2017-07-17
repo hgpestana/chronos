95b16611cfedbc50b0e465d4d678009ed6f03327
h.pestana@novadelphi.com <redleh.figueira.3891>
2017-07-17 20:08:08 +0100

    Class Forms and Views.

--- apps/client/views.py
+++ apps/client/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic.detail import DetailView
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -28,6 +28,21 @@ class IndexView(ListView):
 
 
 class AddView(CreateView):
+    model = TClient
+    form_class = ClientForm
+    template_name = 'client/client_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AddView, self).get_context_data(**kwargs)
+        context['page_title'] = _('Add new client - CHRONOS')
+        context['client_active'] = 'active open'
+        context['client_add_active'] = 'active'
+        context['client_list'] = self.get_queryset()
+
+        return context
+
+
+class EditView(UpdateView):
     model = TClient
     form_class = ClientForm
     template_name = 'client/client_form.html'
