43a33af069ca5e5796b1ca4954d47ebfcef7685e
h.pestana@novadelphi.com <redleh.figueira.3891>
2017-07-30 11:58:39 +0100

    Dashboard development - continued

--- apps/core/views.py
+++ apps/core/views.py
@@ -14,88 +14,95 @@ Main views for the Chronos platform
 
 
 class CoreIndexView(LoginRequiredMixin, TemplateView):
-	"""
-	View that is used to show the Chronos dashboard
-	TODO: Develop this view
-	"""
-	template_name = 'core/dashboard.html'
+    """
+    View that is used to show the Chronos dashboard
+    TODO: Develop this view
+    """
 
-	def get_context_data(self, **kwargs):
-		context = super(CoreIndexView, self).get_context_data(**kwargs)
-		context['page_title'] = _('Dashboard - CHRONOS')
-		context['dashboard'] = 'active'
-		context['totals'] = self.get_totals_user()
-		return context
+    template_name = 'core/dashboard.html'
 
-	def get_totals_user(self):
+    def get_context_data(self, **kwargs):
+        context = super(CoreIndexView, self).get_context_data(**kwargs)
+        context['page_title'] = _('Dashboard - CHRONOS')
+        context['dashboard'] = 'active'
+        context['totals'] = self.get_totals_user()
+        context['last_entry'] = self.get_last_entry()
 
-		totals = {}
+        return context
 
-		try:
-			totals['user_entries'] = Entry.objects.filter(user=self.request.user).count()
-		except Entry.DoesNotExist:
-			totals['user_entries'] = 0
+    def get_last_entry(self):
+        try:
+            return Entry.objects.filter(user=self.request.user).order_by('starttime')[0]
+        except IndexError:
+            return None
 
-		try:
-			totals['user_clients'] = Entry.objects.filter(user=self.request.user).filter(client__isnull=False).values(
-				'client').distinct().count()
-		except Entry.DoesNotExist:
-			totals['user_clients'] = 0
+    def get_totals_user(self):
 
-		try:
-			totals['user_projects'] = Entry.objects.filter(user=self.request.user).filter(project__isnull=False).values(
-				'project').distinct().count()
-		except Entry.DoesNotExist:
-			totals['user_projects'] = 0
+        totals = {}
 
-		try:
-			totals['user_tasks'] = Entry.objects.filter(user=self.request.user).filter(task__isnull=False).values(
-				'task').distinct().count()
-		except Entry.DoesNotExist:
-			totals['user_tasks'] = 0
+        try:
+            totals['user_entries'] = Entry.objects.filter(user=self.request.user).count()
+        except Entry.DoesNotExist:
+            totals['user_entries'] = 0
 
-		totals['entries'] = Entry.objects.all().count()
-		totals['clients'] = Client.objects.all().count()
-		totals['projects'] = Project.objects.all().count()
-		totals['tasks'] = Task.objects.all().count()
+        try:
+            totals['user_clients'] = Entry.objects.filter(user=self.request.user).filter(client__isnull=False).values(
+                'client').distinct().count()
+        except Entry.DoesNotExist:
+            totals['user_clients'] = 0
 
-		try:
-			totals['user_entries_percent'] = (totals['user_entries'] / totals['entries']) * 100
-		except ZeroDivisionError:
-			totals['user_entries_percent'] = 0
+        try:
+            totals['user_projects'] = Entry.objects.filter(user=self.request.user).filter(project__isnull=False).values(
+                'project').distinct().count()
+        except Entry.DoesNotExist:
+            totals['user_projects'] = 0
 
-		try:
-			totals['user_clients_percent'] = (totals['user_clients'] / totals['clients']) * 100
-		except ZeroDivisionError:
-			totals['user_clients_percent'] = 0
+        try:
+            totals['user_tasks'] = Entry.objects.filter(user=self.request.user).filter(task__isnull=False).values(
+                'task').distinct().count()
+        except Entry.DoesNotExist:
+            totals['user_tasks'] = 0
 
-		try:
-			totals['user_projects_percent'] = (totals['user_projects'] / totals['projects']) * 100
-		except ZeroDivisionError:
-			totals['user_projects_percent'] = 0
+        totals['entries'] = Entry.objects.all().count()
+        totals['clients'] = Client.objects.all().count()
+        totals['projects'] = Project.objects.all().count()
+        totals['tasks'] = Task.objects.all().count()
 
-		try:
-			totals['user_tasks_percent'] = (totals['user_tasks'] / totals['tasks']) * 100
-		except ZeroDivisionError:
-			totals['user_tasks_percent'] = 0
+        try:
+            totals['user_entries_percent'] = (totals['user_entries'] / totals['entries']) * 100
+        except ZeroDivisionError:
+            totals['user_entries_percent'] = 0
 
-		totals['user_per_client_entries'] = Entry.objects.all().filter(user=self.request.user).filter(
-			client__isnull=False).values('client__name').annotate(Count('client', distinct=True))
+        try:
+            totals['user_clients_percent'] = (totals['user_clients'] / totals['clients']) * 100
+        except ZeroDivisionError:
+            totals['user_clients_percent'] = 0
 
-		totals['user_per_task_entries'] = Entry.objects.all().filter(user=self.request.user).filter(
-			task__isnull=False).values('task__name').annotate(Count('task', distinct=True))
+        try:
+            totals['user_projects_percent'] = (totals['user_projects'] / totals['projects']) * 100
+        except ZeroDivisionError:
+            totals['user_projects_percent'] = 0
 
-		totals['user_per_project_entries'] = Entry.objects.all().filter(user=self.request.user).filter(
-			project__isnull=False).values('project__name').annotate(Count('project', distinct=True))
+        try:
+            totals['user_tasks_percent'] = (totals['user_tasks'] / totals['tasks']) * 100
+        except ZeroDivisionError:
+            totals['user_tasks_percent'] = 0
 
-		return totals
+        totals['user_per_client_entries'] = Entry.objects.all().filter(user=self.request.user).filter(
+            client__isnull=False).values('client__name').annotate(Count('client', distinct=True))
 
+        totals['user_per_task_entries'] = Entry.objects.all().filter(user=self.request.user).filter(
+            task__isnull=False).values('task__name').annotate(Count('task', distinct=True))
 
+        totals['user_per_project_entries'] = Entry.objects.all().filter(user=self.request.user).filter(
+            project__isnull=False).values('project__name').annotate(Count('project', distinct=True))
+
+        return totals
 
 
 def error404(request):
-	return render(request, 'errors/404.html')
+    return render(request, 'errors/404.html')
 
 
 def error500(request):
-	return render(request, 'errors/500.html')
+    return render(request, 'errors/500.html')
